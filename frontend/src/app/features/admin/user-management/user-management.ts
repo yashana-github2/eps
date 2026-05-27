78bcba9378e3194e0bc5a7c5f9c30ff3fdcc7bb2
yashana-github2 <140480296+yashana-github2@users.noreply.github.com>
2026-05-27 21:29:10 +0530

    Add files via upload

---
+++ frontend/src/app/features/admin/user-management/user-management.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-management',
+  imports: [],
+  templateUrl: './user-management.html',
+  styleUrl: './user-management.scss',
+})
+export class UserManagement {}
